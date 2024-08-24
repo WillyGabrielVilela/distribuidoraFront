c6bbd911d14eb4484435e46b3b599c5232e51ec6
Willy <willygabrielnoval@gmail.com>
2024-08-24 15:18:12 -0300

    config app page not found and home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import Clientes from './pages/Clientes';
 // import Estoque from './pages/Estoque';
 // import Fornecedores from './pages/Fornecedores';
-// import Pedidos from './pages/Pedidos';
-// import Produtos from './pages/Produtos';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/clientes" element={<Clientes />} />
         {/* <Route path="/estoque" element={<Estoque />} /> */}
         {/* <Route path="/fornecedores" element={<Fornecedores />} /> */}
-        {/* <Route path="/pedidos" element={<Pedidos />} /> */}
-        {/* <Route path="/produtos" element={<Produtos />} /> */}
+        <Route path="*" element={<NotFound />} /> {/* Rota para 404 */}
       </Routes>
     </Router>
   );
